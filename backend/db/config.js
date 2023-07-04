30ba5524f0c8076960d60aca389cb1fedbb1dc05
juannnm <103078420+juannnm@users.noreply.github.com>
2023-07-04 21:10:04 +0000

    Implementing React on the Frontend

--- backend/db/config.js
+++ backend/db/config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect("mongodb+srv://Juan:Admin123@shop-database.c8me7kw.mongodb.net/?retryWrites=true&w=majority")
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
